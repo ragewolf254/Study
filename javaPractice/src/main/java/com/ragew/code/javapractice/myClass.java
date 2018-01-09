32b68970905b0088e154ae392c7150277cf1309c
ragewolf254 <ragewolf254@yahoo.com>
2018-01-09 13:05:19 +0800

    Updated no description warning Added a java file for practice purposes

---
+++ javaPractice/src/main/java/com/ragew/code/javapractice/myClass.java
@@ -0,0 +1,17 @@
+package com.ragew.code.javapractice;
+
+public class myClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!, time to buy some rice!");
+        getRice();
+    }
+
+    //Create a function
+    private static void getRice(){
+        System.out.println("Open the door");
+        System.out.println("Go outside");
+        System.out.println("Buy rice from the store at the ground floor");
+        System.out.println("Return home with rice");
+        System.out.println("Close the door");
+    }
+}
